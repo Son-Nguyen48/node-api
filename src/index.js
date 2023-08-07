2a743a88f87a3b6d1563d99901831a45c05e7930
Nguyễn Thế Sơn <55500547+Son-Nguyen48@users.noreply.github.com>
2023-08-07 17:44:13 +0700

    test deploy vercel

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3001;
 const route = require("./routes");
 const db = require("./config/db");
+const { MongoClient } = require("mongodb");
 
 // connect db
 
@@ -38,6 +39,21 @@ app.set("views", path.join(__dirname, "resources", "views"));
 
 //Route init
 route(app);
+
+app.get("/products", async (req, res) => {
+  try {
+    const client = new MongoClient("mongodb://localhost:27017");
+    await client.connect();
+    const db = client.db("F8_blog_dev");
+    const collection = db.collection("products");
+    const data = await collection.find().toArray();
+    res.json(data);
+    client.close();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running ... on port ${port}`);
